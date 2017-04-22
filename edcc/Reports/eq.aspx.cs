467385859eabf139f072aa42b2026c136edb7cf4
mhadji <majidhadji@gmail.com>
2017-04-22 15:43:00 -0700

    WIP

---
+++ edcc/Reports/eq.aspx.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace edcc.Reports
+{
+    public partial class eq : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblSelected.Text = "";
+            lblSelectedPr.Text = "";
+      
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem listItem in ListUniversities.Items)
+            {
+                if (listItem.Selected == true)
+                {
+                    lblSelected.Text += listItem.Text.ToString()+",";
+                }
+            }
+            foreach (ListItem prItem in ListPrograms.Items)
+            {
+                if (prItem.Selected == true)
+                {
+                    lblSelectedPr.Text += prItem.Text.ToString();
+                }
+            }
+
+            ReportViewer1.LocalReport.Refresh();
+         
+        }
+
+    }
+}

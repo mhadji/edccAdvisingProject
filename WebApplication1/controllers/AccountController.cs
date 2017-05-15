16f785c431fb159b152927f6e764a698fd407f1c
mhadji <majidhadji@gmail.com>
2017-05-15 12:51:18 -0700

    modified logoff

--- WebApplication1/controllers/AccountController.cs
+++ WebApplication1/controllers/AccountController.cs
@@ -399,7 +399,7 @@ namespace edccAdvisingProject.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Default.aspx");
         }
 
         //
